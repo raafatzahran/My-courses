a92840fcb195e957bfbfc451b02156bd3764baaa
Raafat Zahran <raafat@telemagic.no>
2018-11-08 21:28:22 +0100

    update gitignore

---
+++ dev/tomcat/apache-tomcat-9.0.12/webapps/ROOT/app/controllers/mainController.js
@@ -0,0 +1,39 @@
+var testApp = angular.module('testApp', []);
+
+testApp.controller('testController' , function ($scope, $http) {
+
+//  Our GET request function
+    $scope.getRequest = function () {
+        console.log("I've been pressed!");
+        id = $scope.userId;
+
+        $http.get("http://localhost:7071/api/users/"+id)
+            .then(function successCallback(response){
+                console.log("Successfully GET-ed data");
+                $scope.response = response;
+            }, function errorCallback(response){
+                console.log("Unable to perform get request");
+            });
+    };
+
+//    Our POST request function
+    $scope.postRequest = function () {
+        bodyReq={
+            "firstName":$scope.firstName,
+            "lastName":$scope.lastName,
+            "email":$scope.email,
+            "password":$scope.password
+        };
+        $http.post("http://localhost:7071/api/users/", bodyReq)
+            .then(function successCallback(response){
+                console.log("Successfully POST-ed data");
+                $scope.firstName='';
+                $scope.lastName='';
+                $scope.email='';
+                $scope.password='';
+            }, function errorCallback(response){
+                console.log("POST-ing of data failed");
+            });
+    };
+
+});
