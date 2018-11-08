3b68f8d98e313930924b03063f8e36ecbae70931
Raafat Zahran <raafat@telemagic.no>
2018-11-08 21:17:32 +0100

    add test

---
+++ mobile-app-ws/src/test/java/com/rfz/app/UserTest.java
@@ -0,0 +1,12 @@
+package com.rfz.app;
+import static org.junit.Assert.*;
+import com.rfz.app.ws.ui.entrypoint.UsersEntryPoint;
+import org.junit.Test;
+
+public class UserTest {
+    @Test
+    public void dummyTest(){
+        System.out.println("Run the test");
+        assertEquals(1,1);
+    }
+}
